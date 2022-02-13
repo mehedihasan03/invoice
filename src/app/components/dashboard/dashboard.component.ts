486f5a1dbb62ab7d23f8e53cce4229f66ff9ee97
mehedihasan03 <mdmehedi03@gmail.com>
2022-02-14 03:07:50 +0600

    edit dshboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  totalSaleOfMonth: any
+  totalCustomer: any
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.totalSaleOfCurrentMonth()
+    this.totalCustomerCount()
   }
 
+  totalSaleOfCurrentMonth() {
+    const headers = { 'content-type': 'application/json' };
+    this.http.get<any>('http://localhost:9988/invoice/current-sale', { headers })
+      .subscribe(map => {
+        this.totalSaleOfMonth = map.Data;
+      })
+  }
+
+  totalCustomerCount() {
+    const headers = { 'content-type': 'application/json' };
+    this.http.get<any>('http://localhost:9988/customer/count-customer', { headers })
+      .subscribe(map => {
+        this.totalCustomer = map.Data;
+      })
+  }
 }
